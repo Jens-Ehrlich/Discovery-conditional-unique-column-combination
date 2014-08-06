01dd000df678110c474167caab59f340c2167877
Jens Hildebrandt <jens.hildebrandt@student.hpi.uni-potsdam.de>
2014-08-06 11:35:00 +0200

    added testcase to ensure plihashmap consistency

--- dcucc/src/test/java/de/uni_potsdam/hpi/metanome/algorithms/dcucc/DcuccSimpleTest.java
+++ dcucc/src/test/java/de/uni_potsdam/hpi/metanome/algorithms/dcucc/DcuccSimpleTest.java
@@ -1,6 +1,24 @@
 package de.uni_potsdam.hpi.metanome.algorithms.dcucc;
 
+import de.uni_potsdam.hpi.metanome.algorithm_helper.data_structures.PLIBuilder;
+import de.uni_potsdam.hpi.metanome.algorithm_helper.data_structures.PositionListIndex;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.input.InputGenerationException;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.input.InputIterationException;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.input.RelationalInput;
+import de.uni_potsdam.hpi.metanome.algorithm_integration.result_receiver.CouldNotReceiveResultException;
+import de.uni_potsdam.hpi.metanome.algorithms.test_helper.fixtures.AbaloneFixture;
+
+import it.unimi.dsi.fastutil.longs.Long2LongOpenHashMap;
+
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Jens Hildebrandt
@@ -13,5 +31,37 @@ public class DcuccSimpleTest {
   public void setUp() throws Exception {
     algorithm = new Dcucc();
     algorithm.conditionLatticeTraverser = new SimpleConditionTraverser(algorithm);
+  }
+
+  @Test
+  public void pliHashmapTest()
+      throws CouldNotReceiveResultException, UnsupportedEncodingException, FileNotFoundException,
+             InputGenerationException, InputIterationException {
+    //Setup
+    AbaloneFixture fixture = new AbaloneFixture();
+    RelationalInput input = fixture.getInputGenerator().generateNewCopy();
+    PLIBuilder builder = new PLIBuilder(input);
+    List<PositionListIndex> pliList = builder.getPLIList();
+
+    List<PositionListIndex> pliListCopy = new ArrayList<>();
+    for (int i = 0; i < pliList.size(); i++) {
+      pliListCopy.add(i, new PositionListIndex(pliList.get(i).getClusters()));
+    }
+
+    for (int i = 0; i < pliList.size(); i++) {
+      PositionListIndex pli = pliList.get(i);
+      PositionListIndex pliCopy = pliListCopy.get(i);
+
+      Long2LongOpenHashMap pliHash = pli.asHashMap();
+      Long2LongOpenHashMap pliCopyHash = pliCopy.asHashMap();
+
+      assertEquals(pliHash, pliCopyHash);
+      assertEquals(pliHash.keySet(), pliCopyHash.keySet());
+
+      for (long row : pliHash.keySet()) {
+        assertEquals(pliHash.get(row), pliCopyHash.get(row));
+      }
+    }
+
   }
 }
