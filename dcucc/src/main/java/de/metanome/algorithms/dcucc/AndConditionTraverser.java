f220c56732e8ce385fe41018be0949a508df1c5d
jens.hildebrandt <jens.hildebrandt@student.hpi.uni-potsdam.de>
2014-10-16 20:38:51 +0200

    added new pruning rule

--- dcucc/src/main/java/de/metanome/algorithms/dcucc/AndConditionTraverser.java
+++ dcucc/src/main/java/de/metanome/algorithms/dcucc/AndConditionTraverser.java
@@ -2,11 +2,14 @@ package de.metanome.algorithms.dcucc;
 
 import de.metanome.algorithm_helper.data_structures.ColumnCombinationBitset;
 import de.metanome.algorithm_helper.data_structures.PositionListIndex;
+import de.metanome.algorithm_helper.data_structures.SubSetGraph;
 import de.metanome.algorithm_integration.AlgorithmExecutionException;
 
 import it.unimi.dsi.fastutil.longs.LongArrayList;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -16,27 +19,61 @@ import java.util.Map;
  */
 public class AndConditionTraverser extends SimpleConditionTraverser {
 
-  //partial unique -> basic column -> cluster
-  protected Map<ColumnCombinationBitset, Map<ColumnCombinationBitset, PositionListIndex>>
+  //partial unique -> basic column -> clusters (one element each)
+  protected Map<ColumnCombinationBitset, Map<ColumnCombinationBitset, LongArrayList>>
       clusterPruningMap;
+  protected SubSetGraph subetSetGraph;
   public AndConditionTraverser(Dcucc algorithm) {
     super(algorithm);
     this.clusterPruningMap = new HashMap<>();
+    this.subetSetGraph = new SubSetGraph();
   }
 
   @Override
   public void iterateConditionLattice(ColumnCombinationBitset partialUnique)
       throws AlgorithmExecutionException {
     Map<ColumnCombinationBitset, PositionListIndex> currentLevel = new HashMap<>();
-
+    boolean prunePartialUnique = true;
     //calculate first level - initialisation
     for (ColumnCombinationBitset conditionColumn : this.algorithm.baseColumn) {
       //TODO better way to prune this columns
       if (partialUnique.containsColumn(conditionColumn.getSetBits().get(0))) {
         continue;
       }
+      PositionListIndex currentConditionPLI = this.algorithm.getPLI(conditionColumn);
+
+      List<ColumnCombinationBitset> traversedSubsets = subetSetGraph.getExistingSubsets(partialUnique);
+      List<LongArrayList> clusters = currentConditionPLI.copy().getClusters();
+      Iterator<LongArrayList> iterator = clusters.iterator();
+      outer:
+      while (iterator.hasNext()) {
+        LongArrayList currentCluster = iterator.next();
+        for (ColumnCombinationBitset traversedSubset : traversedSubsets) {
+          LongArrayList prunedClusters = this.clusterPruningMap.get(traversedSubset).get(conditionColumn);
+          if (prunedClusters == null) {
+            continue;
+          }
+          for (long cluster : prunedClusters) {
+            if (currentCluster.contains(cluster))
+              iterator.remove();
+              continue outer;
+          }
+        }
+      }
+      if (clusters.isEmpty()) {
+        continue;
+      }
+      currentConditionPLI = new PositionListIndex(clusters);
+      prunePartialUnique = false;
+
+
+
       calculateCondition(partialUnique, currentLevel, conditionColumn,
-                         this.algorithm.getPLI(conditionColumn));
+                         currentConditionPLI);
+    }
+
+    if (prunePartialUnique) {
+      this.algorithm.upperPruningGraph.add(partialUnique);
     }
 
     currentLevel = apprioriGenerate(currentLevel);
@@ -75,6 +112,23 @@ public class AndConditionTraverser extends SimpleConditionTraverser {
       List<ConditionEntry> conditionEntries = new LinkedList<>();
       conditionEntries.add(new ConditionEntry(conditionColumn, condition));
       resultSingleton.addMinimalConditionToResult(partialUnique, conditionEntries);
+
+
+      Map<ColumnCombinationBitset, LongArrayList> currentMap = clusterPruningMap.get(partialUnique);
+      if (currentMap == null) {
+        currentMap = new HashMap<>();
+        clusterPruningMap.put(partialUnique, currentMap);
+        subetSetGraph.add(partialUnique);
+      }
+      for (ColumnCombinationBitset oneColumnCondition : conditionColumn.getContainedOneColumnCombinations()) {
+        LongArrayList currentList = currentMap.get(oneColumnCondition);
+        if (currentList == null) {
+          currentList = new LongArrayList();
+          currentList.add(condition.get(0));
+          currentMap.put(oneColumnCondition, currentList);
+        }
+        currentList.add(condition.get(0));
+      }
     }
   }
 }
